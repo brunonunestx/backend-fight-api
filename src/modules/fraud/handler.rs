bcc89c620daecc6bd85593f4f5cad209b41389a6
BrunoNT10 <brunont1211@gmail.com>
2026-05-17 01:28:29 -0300

    fix: release io pool

--- src/modules/fraud/handler.rs
+++ src/modules/fraud/handler.rs
@@ -8,5 +8,8 @@ pub async fn detect_fraud(
     State(app_state): State<Arc<AppState>>,
     Json(payload): Json<Transaction>,
 ) -> Json<FraudResult> {
-    Json(app_state.fraud_service.detect_fraud(&payload))
+    let service = Arc::clone(&app_state.fraud_service);
+    tokio::task::spawn_blocking(move || Json(service.detect_fraud(&payload)))
+        .await
+        .unwrap()
 }
