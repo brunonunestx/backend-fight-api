d35d748682df8284cb994484773e210244ff8290
BrunoNT10 <brunont1211@gmail.com>
2026-05-18 11:19:17 -0300

    fix: nginx conf

--- src/modules/fraud/handler.rs
+++ src/modules/fraud/handler.rs
@@ -8,9 +8,5 @@ pub async fn detect_fraud(
     State(app_state): State<Arc<AppState>>,
     Json(payload): Json<Transaction>,
 ) -> Json<FraudResult> {
-    let service = Arc::clone(&app_state.fraud_service);
-    let result = tokio::task::spawn_blocking(move || service.detect_fraud(&payload))
-        .await
-        .unwrap();
-    Json(result)
+    Json(app_state.fraud_service.detect_fraud(&payload))
 }
